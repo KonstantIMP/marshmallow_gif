33b07f364e1665cf9ecbfddf709b4a0b60c80614
KonstantinProgger <mihedovkos@gmail.com>
2020-03-24 13:59:06 +0300

    Ветка для разработки с отрисовщиком QT

---
+++ CMakeFiles/marshmallow_gif.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/marshmallow_gif.dir/src/app.cpp.o"
+  "CMakeFiles/marshmallow_gif.dir/src/main.cpp.o"
+  "CMakeFiles/marshmallow_gif.dir/src/setting.cpp.o"
+  "CMakeFiles/marshmallow_gif.dir/src/start_setup.cpp.o"
+  "marshmallow_gif"
+  "marshmallow_gif.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/marshmallow_gif.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
